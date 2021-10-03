b0a86c4bf054291e2a547c298f2c6bb36ae7d83c
imjord <dean.jay.goat@gmail.com>
2021-10-03 13:58:11 -0600

    where i left off

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          String stringNum = "2";
+          int toNum = Convert.ToInt32(stringNum);
+          Console.WriteLine(toNum);
+                    }
+    }
+}
